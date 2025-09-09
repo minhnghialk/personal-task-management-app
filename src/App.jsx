be2d03f419e76be286f0e74cc3b998ed61a48efa
minhnghialk <nghia13031995@gmail.com>
2025-09-09 15:00:19 +0700

    chore: initial project setup with Vite and TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./styles/tailwind.css";
+
+export const App = () => {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">Personal Task Management App</h1>
+    </>
+  );
+};
