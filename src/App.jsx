4b9c65e7aae52af2bdfb3d1f1490efd0059d7572
minhnghialk <nghia13031995@gmail.com>
2025-09-24 15:27:52 +0700

    refactor(app): extract BrowserRouter and ToastContainer into AppProviders for cleaner App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
 import React from "react";
 import "./styles/tailwind.css";
-import { BrowserRouter } from "react-router-dom";
+import { AppProviders } from "./AppProviders";
 import { AppInitializer } from "./AppInitializer";
 import { AppRoutes } from "./routes/AppRoutes";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <AppInitializer>
-          <AppRoutes />
-        </AppInitializer>
-      </BrowserRouter>
-
-      {/* Toast container */}
-      <ToastContainer
-        position="top-center"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-    </>
+    <AppProviders>
+      <AppInitializer>
+        <AppRoutes />
+      </AppInitializer>
+    </AppProviders>
   );
 };
 
