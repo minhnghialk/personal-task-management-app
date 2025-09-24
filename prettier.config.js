5fc7fbd5f40fbcd6ba3808e102adf5c83b94d154
minhnghialk <nghia13031995@gmail.com>
2025-09-24 20:04:22 +0700

    chore(prettier): add basic Prettier config

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+export const semi = true;
+export const singleQuote = true;
+export const trailingComma = "all";
+export const printWidth = 100;
+export const tabWidth = 2;
+export const bracketSpacing = true;
+export const arrowParens = "always";
